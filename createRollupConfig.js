89d8cb04b5cbf723e819fc3f01f1981fcd63cd6f
TrasherDK <trasherdk@fumlersoft.dk>
2022-09-29 04:24:54 +0200

    rollup plugins

--- createRollupConfig.js
+++ createRollupConfig.js
@@ -1,17 +1,19 @@
 const cjs = require('@rollup/plugin-commonjs');
-const {resolve} = require('path');
+const { nodeResolve } = require('@rollup/plugin-node-resolve')
+const json = require('@rollup/plugin-json')
+const { resolve } = require('path');
 
 /**
  *
  * @param {string} path
  */
-module.exports = function createRollupConfig(path) {
+module.exports = function createRollupConfig (path) {
   return {
     input: resolve(path, './index.js'),
     output: {
       file: resolve(path, './index.mjs'),
       format: 'esm',
     },
-    plugins: [cjs()],
+    plugins: [nodeResolve({ jsnext: true, main: true }), json(), cjs()],
   };
 };
